3a7b654a64ab7b80de56dd47e9fcd8d248f62aa5
Miguel Angel Moreno <miguel.s.address@gmail.com>
2014-10-08 09:29:14 +0200

    Initial Commit

---
+++ LionsAndTigers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LionsAndTigers
+//
+//  Created by MIGUEL ANGEL MORENO ARMENTEROS on 08/10/14.
+//  Copyright (c) 2014 MIGUEL ANGEL MORENO ARMENTEROS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
