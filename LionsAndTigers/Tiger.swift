64a381978463fb3175281eea7a540dfe755bbc7c
Miguel Angel Moreno <miguel.s.address@gmail.com>
2014-10-11 20:39:23 +0200

    First commit of the project. This app now shows a random tiger image when tapping the tiger button. The random just takes into account that the displayed tiger is not the same as the previous one, so you dont feel the image got stuck or the button does nothing.

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by MIGUEL ANGEL MORENO ARMENTEROS on 09/10/14.
+//  Copyright (c) 2014 MIGUEL ANGEL MORENO ARMENTEROS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var name = ""
+    var age = 0
+    var breed = ""
+    var image = UIImage(named:"")
+}
--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -10,16 +10,84 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var breedLabel: UILabel!
+    
+    //array to hold the tigers
+    //var tigers: Array<Tiger> = []
+    var tigers:[Tiger] = []
+    var lastShownTiger = -1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.initTigersArray()
+        self.setDataFromTiger(getRandomTiger())
+        self.lastShownTiger = 0
+    }
+    
+    func initTigersArray() {
+        var firstTiger = Tiger()
+        firstTiger.name = "Tigger"
+        firstTiger.breed = "Bengal"
+        firstTiger.age = 3
+        firstTiger.image = UIImage(named:"BengalTiger.jpg")
+        
+        var secondTiger = Tiger()
+        secondTiger.name = "Tigress"
+        secondTiger.breed = "Indochinese Tiger"
+        secondTiger.age = 2
+        secondTiger.image = UIImage(named:"IndochineseTiger.jpg")
+        
+        var thirdTiger = Tiger()
+        thirdTiger.name = "Jacob"
+        thirdTiger.breed = "Malayan Tiger"
+        thirdTiger.age = 4
+        thirdTiger.image = UIImage(named:"MalayanTiger.jpg")
+        
+        var fourthTiger = Tiger()
+        fourthTiger.name = "Spar"
+        fourthTiger.breed = "Siberian Tiger"
+        fourthTiger.age = 5
+        fourthTiger.image = UIImage(named:"SiberianTiger.jpg")
+        
+        tigers.append(firstTiger)
+        tigers += [secondTiger, thirdTiger, fourthTiger]
     }
 
+    func setDataFromTiger(tiger:Tiger) {
+        ageLabel.text = "\(tiger.age)"
+        nameLabel.text = tiger.name
+        breedLabel.text = tiger.breed
+        myImageView.image = tiger.image
+    }
+    
+    func _generateRandomNumber( max: Int = 10 )->Int {
+        let randomNumber = Int(arc4random_uniform(UInt32(max)))
+        return randomNumber
+    }
+    
+    func getRandomTiger() -> Tiger {
+        var tigerIndex = _generateRandomNumber(max: tigers.count)
+        while tigerIndex == self.lastShownTiger {
+            tigerIndex = _generateRandomNumber(max: tigers.count)
+        }
+        self.lastShownTiger = tigerIndex
+        return tigers[tigerIndex]
+
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nextBarButtonItemPressed(sender: UIBarButtonItem) {
+        let tiger = self.getRandomTiger()
+        self.setDataFromTiger(tiger)
+    }
 
 }
 
